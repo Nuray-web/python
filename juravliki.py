6e8c453a30b5249b780aa4574edcd70360921e41
Nuray-web <72253666+Nuray-web@users.noreply.github.com>
2021-02-01 22:21:03 +0600

    Create juravliki.py

---
+++ juravliki.py
@@ -0,0 +1,7 @@
+#92
+
+S = int(input("Введите общее количество сделанных журавликов: "))
+a = S // 6 
+Petya = Serezha = str(a) 
+Katya = str(4*a) 
+print("Петя сделал: " + Petya + "\nСережа сделал: " + Serezha + "\nКатя сделала: " + Katya)
