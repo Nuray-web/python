64397534c3d9319db24f30a9306e131d30b80e5b
Nuray-web <72253666+Nuray-web@users.noreply.github.com>
2021-02-11 17:04:26 +0600

    Update LAB2_01.py

--- LAB2_01.py
+++ LAB2_01.py
@@ -5,10 +5,7 @@ print(s[-2])
 print(s[0:5])
 print(s[0:-2])
 print(s[0::2])
-for i in range(len(s)):
-    if i%2 == 1:
-        print(s[i], end = '')
-print("\n" + s[::-1])
-a = s[0::2]
-print(a[::-1])
+print(s[1:-1:2])
+print(s[::-1])
+print(s[::-2])
 print(len(s))
