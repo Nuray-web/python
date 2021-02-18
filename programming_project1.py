de5196923db9d2d799f76c078fc019ff6e4dd887
Nuray-web <72253666+Nuray-web@users.noreply.github.com>
2021-02-18 19:09:15 +0600

    Update programming_project1.py

--- programming_project1.py
+++ programming_project1.py
@@ -1,19 +1,31 @@
 def welcome():
   print("\n▶   What would you like to do?   ◀\n\nOptions:")
-  b = " 1. Show all contacts\n 2. Request for some contact\n 3. Clear contacts\n 4. Add new contacts\n 5. Delete contacts\n 6. Close program\n"
+  b = "   1. Show all contacts\n   2. Add new contact\n   3. Request contact\n   4. Delete contact\n   5. Close program\n"
   return b
 
 def add():
   name = str(input("Please enter the name of the contact: "))
-  num = str(input("Please enter the phone number of the contact: "))
-  contacts[name] = num
+  if name in contacts:
+    print("\nSorry, this contact is already existing!")
+  else:
+    num = str(input("Please enter the phone number of the contact: "))
+    if num in contacts:
+      print("\nSorry, this contact is already exicting!")
+    else:
+      contacts[name] = num
   return contacts
 
 def remove(): 
   rcont = input("Please enter the contact name you want to delete: ")
   if rcont in contacts:
-    del contacts[rcont]
-    return "\nSuccess, your contact has been deleted!"
+    ans = str(input("Please, confirm your action. Type 'True' to confirm or 'False' to prevent action."))
+    if ans == 'True':
+      del contacts[rcont]
+      return "\nSuccess, your contact has been deleted!"
+    elif ans == 'False':
+      print("Your answer is accepted!")
+    else:
+      print("Excuse me, user, your answer is unreadable!")
   else:
     return ("\nSorry, there's no such contact in phonebook")
 
@@ -23,13 +35,6 @@ def call():
     return (contacts.get(callcon))
   else:
     return ("\nSorry, there's no such contact in phonebook")
-
-def clear():
-  if bool(contacts) == False:
-    return "\nSorry, your phonebook is already empty!"
-  else:
-    contacts.clear()
-    return "\nSuccess, your phonebook has been cleared!"
 
 def show():
   if bool(contacts) == False:
@@ -43,26 +48,23 @@ def show():
 def phonebook():
   print(welcome())
   choice = int(input("Please enter the num of the option: "))
-  while choice != 6:
-    if choice == 1:
-      print(show())
-      break
-    elif choice == 4:
-      print(add())
-      break
-    elif choice == 5:
-      print(remove())
-      break
-    elif choice == 2:
-      print(call())
-      break
-    elif choice == 3:
-      print(clear())  
-      break
-    else:
-      break
-  return phonebook()
-
+  if choice<5:
+     while True:
+        if choice == 1:
+          print(show())
+          break
+        elif choice == 2:
+          print(add())
+          break
+        elif choice == 4:
+          print(remove())
+          break
+        elif choice == 3:
+          print(call())
+          break
+     return phonebook()
+  else: 
+    exit()
 
 a = str(input("Please enter your name: "))
 print("\n▶   Welcome to the phonebook app, dear, " + a + "   ◀")
