4d53f88857c578d1f6b3cb26fd9c22d6b9950cef
Nuray-web <72253666+Nuray-web@users.noreply.github.com>
2021-06-06 14:41:32 +0600

    Update зарплата.py

--- зарплата.py
+++ зарплата.py
@@ -1,22 +1,2 @@
-#21
-a = int(input("Введите значение зарплаты первого работника: "))
-b = int(input("Введите значение зарплаты второго работника: "))
-c = int(input("Введите значение зарплаты третьего работника: "))
-
-if (a<b) and (a<c):
-    if (b<c):
-        print("Их разница равна "+ str(c-a))
-    else:
-        print("Их разница равна "+ str(b-a))
-if (b<a) and (b<c):
-    if(a<c):
-        print("Их разница равна "+ str(c-b))
-    else:
-        print("Их разница равна "+ str(a-b))
-if (c<a) and (c<b):
-    if (a<b):
-        print("Их разница равна "+ str(b-c))
-    else:
-        print("Их разница равна "+ str(a-c))
-if (a==c) and (a==b):
-    print ("Их разница равна 0")
+a,b,c = list(map(int, input().split()))
+print(max(max(a,b),c) - min(min(a,b),c))
