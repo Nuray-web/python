f5842fb97ac91baa272ae598903a4b2c475bf134
Nuray-web <72253666+Nuray-web@users.noreply.github.com>
2021-06-06 14:57:10 +0600

    Update баскетбол.py

--- баскетбол.py
+++ баскетбол.py
@@ -1,31 +1,10 @@
-#61
-print("Please, enter the points first team got: ")
-l = []
-a = int(input())
-l.append(a)
-b = int(input())
-l.append(b)
-c = int(input())
-l.append(c)
-d = int(input())
-l.append(d)
-
-print("Please, enter the points second team got: ")
-s = []
-e = int(input())
-s.append(e)
-f = int(input())
-s.append(f)
-g = int(input())
-s.append(g)
-h = int(input())
-s.append(h)
-
-i = a+b+c+d
-j = e+f+g+h
-if i>j:
-    print("Won: 1")
-elif i<j:
-    print("Won: 2")
+a,b = list(map(int,input().split()))
+c,d = list(map(int,input().split()))
+e,f = list(map(int,input().split()))
+g,h = list(map(int,input().split()))
+if (a+c+e+g)>(b+d+f+h):
+    print(1)
+elif (a+c+e+g)<(b+d+f+h):
+    print(2)
 else:
     print("DRAW")
