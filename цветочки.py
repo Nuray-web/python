c1458839ee969f2d9508901a310bc8fa4b86f5ce
Nuray-web <72253666+Nuray-web@users.noreply.github.com>
2021-08-04 15:40:30 +0600

    Update цветочки.py

--- цветочки.py
+++ цветочки.py
@@ -1,16 +1,6 @@
-#504
-k = int(input("Please enter the value: "))
-#GCV - 1  VCG - 4  GCV - 7
-#GVC - 2  CVG - 5  GVC - 8
-#VGC - 3  CGV - 6  ...
-f = 'GCV'
-fl = list(f)
-for i in range(k):
-    a = fl[1]
-    fl[1] = fl[2]
-    fl[2] = a
-    a = fl[1]
-    fl[1] = fl[0]
-    fl[0] = a
-    i += 1
-print(str(fl))
+n = int(input())
+s = ['G', 'C', 'V']
+for i in range(n):
+    s[2], s[1] = s[1], s[2]
+    s[0], s[1] = s[1], s[0]
+print(s[0]+s[1]+s[2])
