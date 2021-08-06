b7bb835d683d78889e2eb0ad9c0108bd94d9efbd
Nuray-web <72253666+Nuray-web@users.noreply.github.com>
2021-08-06 18:32:37 +0600

    Update день_программиста.py

--- день_программиста.py
+++ день_программиста.py
@@ -1,6 +1,24 @@
 #550
-n = int(input("Please enter the value of year: "))
-if n%400 == 0 or n%100!=0 and n%4==0: 
-    print("12/09/"+str(n))
+n = int(input())
+if n%400 == 0 or (n%100!=0 and n%4==0): 
+    if n < 10:
+            print("12/09/000")
+    else:
+        if n<100:
+            print("12/09/00" + str(n))
+        else:
+            if n<1000:
+                print("12/09/0"+ str(n))
+            else:
+                print("12/09/"+ str(n))
 else:
-    print("13/09/"+str(n))
+    if n < 10:
+            print("13/09/000"+ str(n))
+    else:
+        if n<100:
+            print("13/09/00"+ str(n))
+        else:
+            if n<1000:
+                print("13/09/0"+ str(n))
+            else:
+                print("13/09/"+ str(n))
